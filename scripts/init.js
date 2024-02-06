d10c0a3194d759eec814fb42b5961036b844a7a5
IhorProgrammer <ihor.kuzubov@gmail.com>
2024-02-06 22:08:05 +0200

    first commit

---
+++ scripts/init.js
@@ -0,0 +1,24 @@
+(function($){
+    $(function(){
+        // $('.parallax').parallax();
+        $('.sidenav').sidenav(); 
+        $('.dropdown-trigger').dropdown(
+            {
+                "coverTrigger": false,
+                "alignment": "center",
+            }
+                
+        );
+        $('.bgn-slider').slick({
+            infinite: true,
+            speed: 1000,
+            fade: true,
+            cssEase: 'linear',
+            autoplay: true,
+            autoplaySpeed: 5000,
+            arrows: false,
+            
+        });
+    }); 
+  })(jQuery);
+  
